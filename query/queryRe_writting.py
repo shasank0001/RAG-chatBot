0984fcff7351c82f7a92623fce381e58ad3aa2a7
shasank0001 <shasank12344@gmail.com>
2025-03-17 20:45:43 +0530

    update

--- query/queryRe_writting.py
+++ query/queryRe_writting.py
@@ -10,7 +10,7 @@ from data_processing.DB import vector_store_retriever
 def re_write_query(query):
     template = """You are an AI language model assistant. Your task is to generate five 
     different versions of the given user question to retrieve relevant documents from a vector 
-    database of text books and pdfs. By generating multiple perspectives on the user question, your goal is to help
+    database of text books and pdfs if the quetions are not needed just give the same quetion. By generating multiple perspectives on the user question, your goal is to help
     the user overcome some of the limitations of the distance-based similarity search. 
     Provide these alternative questions separated by newlines. Original question: {query}"""
 
